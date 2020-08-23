5d47c48d0ff62b2a47008e9c99e1c6e194f22af2
kye <kai.ye@marklogic.com>
2020-08-23 15:50:23 -0700

    Add comment

--- pygame template.py
+++ pygame template.py
@@ -1,10 +1,11 @@
 # Pygame template - skeleton for a new pygame project
+# pip install pygame
 import pygame
 import random
 
 WIDTH = 360
 HEIGHT = 480
-FPS = 30
+FPS = 30     # frame per second
 
 # define colors
 WHITE = (255, 255, 255)
@@ -15,7 +16,7 @@ BLUE = (0, 0, 255)
 
 # initialize pygame and create window
 pygame.init()
-pygame.mixer.init()
+pygame.mixer.init()      #init sound
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
